c5ce1a42bd0e9aaa7d0fa03b18917751c1d8b98c
Paul Pasternak <paulpastnak@gmail.com>
2024-11-20 00:46:35 -0500

    world.ui + block.json start

--- app.cpp
+++ app.cpp
@@ -641,34 +641,7 @@ void genRectangleEBO(const std::array<float, 120> vertices, const std::array<int
     16, 17, 18, 16, 18, 19,
     // Face 6
     20, 21, 22, 20, 22, 23
-  };
-
-  unsigned int VAO, VBO, EBO;
-  glGenVertexArrays(1, &VAO);
-  glGenBuffers(1, &VBO);
-  glGenBuffers(1, &EBO); 
-
-  glBindVertexArray(VAO);
-
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
-
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
- 
-
-  glBindVertexArray(VAO);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  // position attribute
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-  glEnableVertexAttribArray(0);
-  // color attribute
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-  glEnableVertexAttribArray(1);
-  //texture attribute
-  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-  glEnableVertexAttribArray(2);
-*/
+  };*/
 
   glm::mat4 model = glm::mat4(1.0f);
   glm::vec3 translation(location[0], location[1], location[2]);
